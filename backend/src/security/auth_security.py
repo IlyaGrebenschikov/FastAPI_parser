00add0a09759a43c3de546e8b31fbfc0ed4201b1
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-06-08 18:58:47 +0400

    add auth security

---
+++ backend/src/security/auth_security.py
@@ -0,0 +1,15 @@
+from passlib.context import CryptContext
+
+from fastapi.security import OAuth2PasswordBearer
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password) -> str:
+    return pwd_context.hash(password)
