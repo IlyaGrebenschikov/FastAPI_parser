ba3b7ca856dd4c4bc83471d22091957e3b7fc6df
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-06-08 21:51:47 +0400

    add auth router

--- backend/src/api/v1/routers.py
+++ backend/src/api/v1/routers.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from backend.src.api.v1.user.routers import user_router
+from backend.src.api.v1.auth.routers import auth_router
 
 
 v1_router = APIRouter(
     prefix='/v1',
 )
 v1_router.include_router(user_router)
+v1_router.include_router(auth_router)
